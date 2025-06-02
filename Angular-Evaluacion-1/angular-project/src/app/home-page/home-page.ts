308b02c9bf868784c5751acfb39c25168b0b58ee
Juancho1403 <juanandresr000@gmail.com>
2025-06-01 21:06:39 -0400

    Add components for blog, destination, footer, packages, registration, service, team, and testimonial sections
    - Created Blog component with associated HTML and CSS files.
    - Created Destination component with associated HTML and CSS files, including Bootstrap and Font Awesome links.
    - Created Footer component with associated HTML and CSS files, including social media links and a newsletter signup.
    - Created Packages component with associated HTML and CSS files, showcasing various travel packages.
    - Created Registration component with associated HTML and CSS files for user sign-up.
    - Created Service component with associated HTML and CSS files, detailing travel services offered.
    - Created Team component with associated HTML and CSS files, displaying travel guides.
    - Created Testimonial component with associated HTML and CSS files, featuring client testimonials.

--- Angular-Evaluacion-1/angular-project/src/app/home-page/home-page.ts
+++ Angular-Evaluacion-1/angular-project/src/app/home-page/home-page.ts
@@ -3,10 +3,17 @@ import { Banner } from '../banner/banner';
 import { Booking } from '../booking/booking';
 import { About } from '../about/about';
 import { Features } from '../features/features';
-
+import { Destination } from '../destination/destination';
+import { Service } from '../service/service';
+import { Packages } from '../packages/packages';
+import { Registration } from '../registration/registration';
+import { Team } from '../team/team';
+import { Testimonial } from '../testimonial/testimonial';
+import { Blog } from '../blog/blog';
+import { Footer } from '../footer/footer';
 @Component({
   selector: 'app-home-page',
-  imports: [Banner, Booking, About, Features],
+  imports: [Banner, Booking, About, Features, Destination, Service,Packages, Registration, Team, Testimonial, Blog, Footer],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
