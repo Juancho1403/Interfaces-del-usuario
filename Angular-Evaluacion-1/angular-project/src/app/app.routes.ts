3c6c9064af3224b59ad0f7b3ef2948979261efac
Farrauto <jvfarrauto06@gmail.com>
2025-06-03 14:41:58 -0400

    route configuration

--- Angular-Evaluacion-1/angular-project/src/app/app.routes.ts
+++ Angular-Evaluacion-1/angular-project/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
 import { Login } from './login/login';
+import { FormFull } from './form-full/form-full';
 
 export const routes: Routes = [
-  { path: '', component: Login },         // Login es la ruta principal
-  { path: 'home', component: HomePage },  // Home accesible luego del login
-  { path: '**', redirectTo: '' }          // Redirección para rutas no válidas (opcional)
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomePage},
+    {path: 'login', component: Login},
+    {path: 'form', component: FormFull},
 ];
-
